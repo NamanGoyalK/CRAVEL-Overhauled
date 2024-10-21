4d542b7b8bc0971f63ee50851172e10d3c4289e4
Naman Goyal <namangoyaltms@outlook.com>
2024-10-21 13:35:49 +0530

    coming soon

--- cravel/lib/Pages/SubPages/density_page.dart
+++ cravel/lib/Pages/SubPages/density_page.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
 Widget densityPage() {
-  return const Center(
-    child: Text('Density Page'),
+  return Center(
+    child: TweenAnimationBuilder(
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOutCubic,
+      tween: Tween<double>(begin: 0.0, end: 175.0),
+      builder: (context, double size, child) {
+        return Container(
+          width: double.infinity / 2,
+          height: size * 1,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: const Color.fromARGB(255, 51, 81, 231),
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(255, 51, 81, 231),
+                blurRadius: size * 2,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              'Coming\nSoon !',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: const Color.fromARGB(233, 255, 255, 255),
+                fontSize: size * 0.11,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        );
+      },
+    ),
   );
 }
